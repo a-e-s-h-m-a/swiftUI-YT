e26ee1cea3637deda1ef3e9548da8fd3709c5ac1
Aeshma <madhushanka2727@gmail.com>
2025-04-12 14:02:05 +0530

    initial commit

---
+++ YT-Exp/YT-Exp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YT-Exp
+//
+//  Created by Malith Madhushanka on 2025-04-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YT-Exp/YT-Exp/YT_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_ExpApp.swift
+//  YT-Exp
+//
+//  Created by Malith Madhushanka on 2025-04-12.
+//
+
+import SwiftUI
+
+@main
+struct YT_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
